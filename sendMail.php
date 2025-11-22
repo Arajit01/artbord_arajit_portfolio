ae57f21f1ea18c5303dc153ba71ed9d7549a30fa
Arajit Halder <102661324+Arajit01@users.noreply.github.com>
2025-11-23 02:50:47 +0530

    Add files via upload

---
+++ sendMail.php
@@ -0,0 +1,22 @@
+<?php
+$to = "YOUR_EMAIL@gmail.com";
+
+$date = $_POST['selectedDate'];
+$time = $_POST['selectedTime'];
+$message = $_POST['message'];
+$email = $_POST['email'];
+
+$subject = "New Booking Request";
+
+$body = "Booking Details:\n
+Date: $date\n
+Time: $time\n
+Email: $email\n
+Message: $message\n";
+
+$headers = "From: $email";
+
+mail($to, $subject, $body, $headers);
+
+echo "Booking Successful! I will contact you soon.";
+?>
